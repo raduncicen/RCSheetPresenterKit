b0fc14f517b0a43b4899b210044bffbf7a4d7a27
Radun Çiçen <raduncicen@gmail.com>
2025-02-25 10:17:39 +0300

    RCPreviewKit - versioning

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["RCSheetPresenterKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/raduncicen/RCPreviewKit", .branchItem("main"))
+        .package(url: "https://github.com/raduncicen/RCPreviewKit", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
